3961fae9a730ef434c4f34d6a4e1754bf9203ed0
Rittin Shingari <61085942+rittin97@users.noreply.github.com>
2020-02-28 18:34:14 -0500

    Add files via upload

---
+++ lab3/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab3
+{
+    public class Controller
+    {
+        private String path = @"db.txt";
+        public String FilePath
+        {
+            get { return path; }
+
+            set { path = value; }
+        }
+
+        //public String PassCode { get; }
+
+        //public String UserName { get; }
+
+        public static int counter = 0;
+
+    }
+}
